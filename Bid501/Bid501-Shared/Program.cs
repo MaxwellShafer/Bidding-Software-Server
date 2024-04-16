dd28484bb56ceefac64515a88282335e30ffd0e3
cbergdall <cbergdall@ksu.edu>
2024-04-16 10:05:18 -0500

    initial commit

---
+++ Bid501/Bid501-Shared/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bid501_Shared
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
