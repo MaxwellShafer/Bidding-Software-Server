679c8a19ddf59e928fe7483be1440d4a4a503f3c
Gabriel McReynolds <gabrielmc785@gmail.com>
2024-04-16 10:29:35 -0500

    client login

---
+++ Bid501/Bid501-Client/ClientCommCtrl.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using WebSocketSharp;
+
+namespace Bid501_Client
+{
+    public delegate bool Message(string message);
+
+    public class ClientCommCtrl
+    {
+        private WebSocket ws;
+        
+        // Event for when a message is received from the server
+        public event Message MessageReceived;
+
+        public ClientCommCtrl()
+        { // Connects to the server
+            ws = new WebSocket("ws://127.0.0.1:8001/server");
+            ws.OnMessage += (sender, e) => { if (MessageReceived != null) MessageReceived(e.Data); };
+            ws.Connect();
+        }
+
+        public bool Login(String username, String password)
+        {
+            return true;
+        }
+    }
+}
