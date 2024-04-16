ce70ef7d61f5c70e52d878d8e3c5fa3924969028
cbergdall <cbergdall@ksu.edu>
2024-04-16 10:45:34 -0500

    added websocket

--- Bid501/Bid501-Server/Program.cs
+++ Bid501/Bid501-Server/Program.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace Bid501_Server
 {
@@ -16,7 +17,8 @@ namespace Bid501_Server
         [STAThread]
         static void Main()
         {
-
+            WebSocketServer serverSocket = new WebSocketServer(8001);
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
