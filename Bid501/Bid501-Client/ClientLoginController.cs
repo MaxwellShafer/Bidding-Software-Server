c0808dcd4ff1523ec637f3ca8e2494ab9f3fdeea
YenneEric <esyenne@ksu.edu>
2024-04-17 10:36:35 -0500

    class added

---
+++ Bid501/Bid501-Client/ClientLoginController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Client
+{
+    internal class ClientLoginController
+    {
+    }
+}
