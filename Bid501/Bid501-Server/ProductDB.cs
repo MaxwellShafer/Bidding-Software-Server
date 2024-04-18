aedb3e9dae64585228e7d6ee3cc515447820d658
Max Shafer <75923107+MaxwellShafer@users.noreply.github.com>
2024-04-18 10:38:40 -0500

    Added Product DB and IDB, IDB currently is blank and needs to be considered and developed further

---
+++ Bid501/Bid501-Server/ProductDB.cs
@@ -0,0 +1,16 @@
+﻿using Bid501_Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Server
+{
+    public class ProductDB : IDB
+    {
+        public List<Product> Products { get; set; }
+
+    }
+}
