068e3087378a95c9e93ac73550fb724fbea9f1e1
Charlie Bergdall <charliusb@gmail.com>
2024-05-01 10:49:32 -0500

    Expired Bid

--- Bid501/Bid501-Server/ServerCommCtrl.cs
+++ Bid501/Bid501-Server/ServerCommCtrl.cs
@@ -1,4 +1,5 @@
 ﻿using Bid501_Shared;
+using Bid501_Shared.dto;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Windows.Forms;
@@ -39,6 +40,30 @@ namespace Bid501_Server
                     break;
                 default:
                     break;
+            }
+        }
+
+        /// <summary>
+        /// Handles when a bid expires
+        /// </summary>
+        /// <param name="p"></param>
+        public void HandleExpiringBid(IProduct p)
+        {
+            string clientId = GetId(((Product)p).User);
+            BidExpiredDTO dto = new BidExpiredDTO();
+            dto.Id = p.Id;
+            dto.IsWinning = true;
+            dto.Bid = p.MinBid;
+            Sessions.SendTo(dto.Serialize(), clientId);
+
+            dto.IsWinning = false;
+            string serialized = dto.Serialize();
+            foreach (string id in Sessions.IDs)
+            {
+                if (id != clientId)
+                {
+                    Sessions.SendTo(serialized, id);
+                }
             }
         }
     }
